6bee59df5c34c546b6200a9c29d48be88985627e
Majd Alfhaily <majd@alfhaily.me>
2022-03-05 19:18:28 +0100

    Add implementation

--- main.go
+++ main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+)
+
+func getEnv(key string, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	ip, _, err := net.SplitHostPort(req.RemoteAddr)
+
+	if err == nil {
+		fmt.Fprintf(w, "%s\n", ip)
+	} else {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	port := getEnv("PORT", "3000")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+}
